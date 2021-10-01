a1932909d77162915b4b73f09800d5618afaa8d2
MrXaid <91692910+MrXaid@users.noreply.github.com>
2021-10-01 23:21:52 +0530

    git commit

---
+++ speak.py
@@ -0,0 +1,24 @@
+def speak(str):
+    from win32com.client import Dispatch
+    speak = Dispatch("SAPI.spVoice")
+    speak.speak(str)
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen
+
+op = urlopen('https://news.google.com/news/rss')
+rd = op.read()
+op.close()
+sp_page = soup(rd, "html.parser")
+news_list = sp_page.find_all("item")
+for news in news_list:
+    a = news.title.text
+    b = news.link.text
+    print(news.title.text)
+    print(news.link.text)
+    #print(news.pubDate.text)
+    print('-' * 60)
+    speak(a)
+    speak(b)
+
+
+
